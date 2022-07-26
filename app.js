ae3d53e0a774e1105f2ca9322457f5a9a3a42836
wawerusamuel <waweruzamuel@gmail.com>
2022-07-26 12:07:23 +0300

    changed the functions to  es6 functions..

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const startButton = document.querySelector(".start");
 const stopButton = document.querySelector(".stop");
-renderTime();
 let interval;
 
-startButton.addEventListener("click", startTime);
-stopButton.addEventListener("click", stopTime);
+const startTime = () => (interval = setInterval(renderTime, 1000));
 
-function startTime() {
-  interval = setInterval(renderTime, 1000);
-}
-
-function renderTime() {
+const renderTime = () => {
   const timeDisplay = document.querySelector(".display h5");
   timeDisplay.innerText = new Date();
-}
+};
+const stopTime = () => clearInterval(interval);
 
-function stopTime() {
-  clearInterval(interval);
-}
+renderTime();
+startButton.addEventListener("click", startTime);
+stopButton.addEventListener("click", stopTime);
